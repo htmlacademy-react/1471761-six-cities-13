98b7b77f97c0379509da15ca6dd01531e3d9731f
Luiza Leahu <leahuluiza@gmail.com>
2023-07-13 14:19:29 +0200

    add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,44 @@
 import MainPage from '../../pages/main-page/main-page';
 
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+
 type AppScreenProps = {
   offersCount: number;
 }
 
 function App({ offersCount }: AppScreenProps): JSX.Element {
   return (
-    <MainPage offersCount={offersCount} />
+
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,1 +1,6 @@
-
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id'
+}
